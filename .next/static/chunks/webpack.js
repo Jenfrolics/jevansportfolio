2f70829913875c5a89d06486ca6f3149afb5c7c9
Jenfrolics <jenn.evans@croquet.io>
2023-08-22 11:13:28 -0700

    remove stuff i dont need

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4923c3bf51b09e79"; }
+/******/ 		__webpack_require__.h = function() { return "7b60be97068ba23b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
